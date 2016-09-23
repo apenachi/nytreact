a43ed49d9330c319bcfef792de7f8bb19fc6b806
Alexander Penachi <apenachi@yahoo.com>
2016-09-23 15:52:37 -0400

    Update SavedPage.jsx

--- app/Pages/SavedPage.jsx
+++ app/Pages/SavedPage.jsx
@@ -10,14 +10,10 @@ class SavedPage extends Component {
 
 	render() {
 		console.log('Saved Page', this.props)
-		const { articles, saveArticle, savedArticles } = this.props;
+		const { savedArticles } = this.props;
 
 		return (
 			<div className="SavedPage text-center">
-				<div className="row">
-					<h1>These articles were found ....</h1>
-					<ArticleList articles={articles} saveArticle={saveArticle}/>
-				</div>
 				<div className="row">
 					<History savedArticles={savedArticles}/>
 				</div>
